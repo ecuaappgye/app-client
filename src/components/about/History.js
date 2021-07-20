7dffd9769ca9bd70934adc6e34da6f4d7a89ceb5
ecuaappgye <87317571+ecuaappgye@users.noreply.github.com>
2021-07-20 17:01:39 -0500

    UptateHis

--- src/components/about/History.js
+++ src/components/about/History.js
@@ -1,7 +1,6 @@
 import React from "react";
 import About from "./About.css";
-import MisionI from "../../assets/img/misionwuy.png";
-import ImageDesem from "../../assets/img/ImagenDesem.png";
+import IHistory from "../../assets/img/history.png";
 
 const History = () => {
   return (
@@ -13,20 +12,20 @@ const History = () => {
           <div className="row">
             <div className="col-lg 6 align-self-center p-3">
               <div className="">
-                <img src={MisionI} className="img-fluid" alt="Nuestra Misión" />
+                <img src={IHistory} className="img-fluid" alt="Nuestra Misión" />
               </div>
             </div>
             <div className="col-lg-6 align-content-center p-4">
               <div className=" frameDegra border-radius-p text-start text-dark  p-5">
                 <h2>Historia</h2>
-                <p className="fs-5">
+                <p className="fs-6">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Exercitationem voluptatum nisi iste ducimus quasi voluptatibus
                   recusandae molestiae placeat. Accusantium, natus repellat
                   magni facilis vero accusamus dolores hic? Repudiandae, at
                   culpa.
                 </p>
-                <p className="fs-5">
+                <p className="fs-6">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Temporibus, unde officia? Itaque dolorem amet odio iste
                   consequatur saepe. Impedit inventore tempora quisquam eius
